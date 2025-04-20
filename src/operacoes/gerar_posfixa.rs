efdbe21b480c8ff7dd2fad06bf7ac528abd9ee13
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2025-04-20 17:56:42 -0300

    Refactor infixa_para_posfixa to return String and enhance token processing logic

--- src/operacoes/gerar_posfixa.rs
+++ src/operacoes/gerar_posfixa.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 struct Stack {
     size: i32,
     data: Vec<String>,
@@ -32,13 +34,89 @@ impl Stack {
     }
 }
 
-pub fn infixa_para_posfixa(expressao: &str) -> &str {
+pub fn infixa_para_posfixa(expressao: &str) -> String {
     let binding = expressao.replace("(", " ( ").replace(")", " ) ");
     let tokens: Vec<&str> = binding.split_whitespace().collect();
-    println!("Tokens: {:?}", tokens);
 
     let mut pilha = Stack::new(100);
     let mut saida: Stack = Stack::new(100);
 
-    return expressao;
+    let tokens_assoc = HashMap::from([
+        ("(", 0),
+        (")", 0),
+        ("+", 1),
+        ("-", 1),
+        ("*", 2),
+        ("/", 2),
+        ("^", 3),
+    ]);
+
+    for token in tokens {
+        println!("Analisando Token: {}", token);
+        if !tokens_assoc.contains_key(token) {
+            saida.push(token.to_string());
+            println!("Pilha: {:?}", pilha.data);
+            println!("Saída: {:?}", saida.data);
+            continue;
+        }
+
+        if token == "(" {
+            pilha.push(token.to_string());
+            println!("Pilha: {:?}", pilha.data);
+            println!("Saída: {:?}", saida.data);
+            continue;
+        }
+
+        if token == ")" {
+            let mut ultimo_elemento = pilha.top();
+
+            while ultimo_elemento.is_some() && ultimo_elemento.unwrap() != "(" {
+                saida.push(ultimo_elemento.unwrap().to_string());
+                pilha.pop();
+                ultimo_elemento = pilha.top();
+            }
+            if ultimo_elemento.is_some() && ultimo_elemento.unwrap() == "(" {
+                pilha.pop(); // Remove o "(" da pilha
+            }
+            println!("Pilha: {:?}", pilha.data);
+            println!("Saída: {:?}", saida.data);
+            continue;
+        }
+
+        if tokens_assoc.contains_key(token) {
+            let prioridade_token = tokens_assoc.get(token).unwrap();
+            if pilha.top().is_some() {
+                let mut ultimo_elemento = pilha.top().unwrap();
+                let mut prioridade_ultimo_elemento =
+                    tokens_assoc.get(ultimo_elemento.as_str()).unwrap();
+
+                while prioridade_token <= prioridade_ultimo_elemento {
+                    saida.push(ultimo_elemento.to_string());
+                    pilha.pop();
+                    ultimo_elemento = pilha.top().unwrap();
+                    prioridade_ultimo_elemento =
+                        tokens_assoc.get(ultimo_elemento.as_str()).unwrap();
+                }
+            }
+            pilha.push(token.to_string());
+        }
+        println!("Pilha: {:?}", pilha.data);
+        println!("Saída: {:?}", saida.data);
+    }
+
+    println!("-> Desempilhando resto");
+    while !pilha.data.is_empty() {
+        let ultimo_elemento = pilha.top().unwrap();
+        if ultimo_elemento != "(" {
+            saida.push(ultimo_elemento.to_string());
+            pilha.pop();
+        } else {
+            pilha.pop(); // Remove o "(" da pilha
+        }
+    }
+
+    println!("Pilha: {:?}", pilha.data);
+    println!("Saída: {:?}", saida.data);
+
+    return saida.data.join(" ");
 }
